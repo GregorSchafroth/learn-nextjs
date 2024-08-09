c4306ec9d64673ac62462ce5564c620f5c9bcc77
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-09 16:42:00 +0200

    add photoswipe lightbox gallery

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -38,7 +38,7 @@ const PropertyPage = async ({ params }) => {
         </div>
       </section>
       <section className='bg-blue-50'>
-        <div className='container m-auto py-10 px-6'>
+        <div className='container m-auto pt-10 pb-4 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             <PropertyDetails property={property} />
             <aside className="space-y-4">
@@ -50,7 +50,7 @@ const PropertyPage = async ({ params }) => {
         </div>
       </section>
       <div className='bg-blue-50'>
-        <div className='container m-auto py-10 px-6'>
+        <div className='container m-auto pb-10 pt-4 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             <PropertyImages images={property.images} />{' '}
           </div>
