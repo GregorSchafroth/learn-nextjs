c624b42602fcf436489f062e15b6abee178c5b10
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-10 14:16:04 +0200

    fix profile overflow on mobile

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -41,7 +41,7 @@ const PropertyPage = async ({ params }) => {
         <div className='container m-auto pt-10 pb-4 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             <PropertyDetails property={property} />
-            <aside className="space-y-4">
+            <aside className='space-y-4'>
               <BookmarkButton property={property} />
               <ShareButtons property={property} />
               <PropertyContactForm property={property} />
--- utils/convertToObject.js
+++ utils/convertToObject.js
@@ -1,6 +1,14 @@
 export function convertToSerializableObject(leanDocument) {
+  if (!leanDocument) {
+    return;
+  }
+
   for (const key of Object.keys(leanDocument)) {
-    if (leanDocument[key].toJSON && leanDocument[key].toString) {
+    if (
+      leanDocument[key] &&
+      leanDocument[key].toJSON &&
+      leanDocument[key].toString
+    ) {
       leanDocument[key] = leanDocument[key].toString();
     }
   }
